786359388fefd2938b8dec595a9eb0ff5e64e976
Abul Hashem Jr <abul@limosys.com>
2020-03-31 15:43:27 -0400

    First commit

---
+++ app/src/main/java/com/project/covid19/Covid19App.kt
@@ -0,0 +1,9 @@
+package com.project.covid19
+
+import android.app.Application
+
+class Covid19App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/project/covid19/MainActivityCovid19.kt
@@ -0,0 +1,12 @@
+package com.project.covid19
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityCovid19 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
